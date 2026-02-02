fadfe09567cace18399d5439df0a62e2b63b3379
Josh Liddy <jliddy@umass.edu>
2026-02-02 15:38:08 -0500

    Completed exports scripts for reaching and rsvp data.

---
+++ Export_RSVP.m
@@ -0,0 +1,36 @@
+% Clear variables and command window.
+clear; clc;
+
+% Load data
+load(fullfile("Data","Results.mat"));
+
+% Select cycle indices:
+% baseline = cycles  6-10
+% onset    = cycles 11-12
+% early    = cycles 13-17
+% late     = cycles 46-50
+cycle_idx = {6:10, 11:12, 13:17, 46:50};
+
+% Extract variables
+%#ok<*AGROW>
+id = [];
+group = [];
+cycle = [];
+phase = [];
+accuracy = [];
+
+for i = 1:height(DataTable)
+    id = [id; repmat(DataTable.PartID(i),length(cycle_idx),1)];
+    group = [group; repmat(DataTable.Group(i),length(cycle_idx),1)];
+    phase = [phase; ["baseline"; "onset"; "early"; "late" ]];
+    part_accuracy = [mean(DataTable.Accuracy_Cycle{i}(cycle_idx{1}),"omitmissing"); ...
+                     mean(DataTable.Accuracy_Cycle{i}(cycle_idx{2}),"omitmissing"); ...
+                     mean(DataTable.Accuracy_Cycle{i}(cycle_idx{3}),"omitmissing"); ...
+                     mean(DataTable.Accuracy_Cycle{i}(cycle_idx{4}),"omitmissing")];
+    accuracy = [accuracy; part_accuracy];
+end
+
+% Export data for statistical analyses
+T = table(id, group, phase, accuracy, ...
+          'VariableNames',{'id', 'group', 'phase', 'accuracy'});
+writetable(T, fullfile(cd, "Statistics", "Data", "Data_RSVP.xlsx"));
---
+++ Export_Reaching.m
@@ -0,0 +1,34 @@
+% Clear variables and command window.
+clear; clc;
+
+% Load data
+load(fullfile("Data","Results.mat"));
+
+% Select cycle indices
+cycle_idx = 1:51;
+
+% Extract variables
+%#ok<*AGROW>
+id = [];
+group = [];
+cycle = [];
+ha = [];
+rt = [];
+mt = [];
+
+for i = 1:height(DataTable)
+    id = [id; repmat(DataTable.PartID(i),length(cycle_idx),1)];
+    group = [group; repmat(DataTable.Group(i),length(cycle_idx),1)];
+    cycle = [cycle; cycle_idx'];
+    ha = [ha; DataTable.HA_Cycle{i}(cycle_idx)];
+    rt = [rt; DataTable.RT_Cycle{i}(cycle_idx)];
+    mt = [mt; DataTable.MT_Cycle{i}(cycle_idx)];
+end
+
+% Total action duration
+tad = rt + mt;
+
+% Export data for statistical analyses
+T = table(id, group, cycle, ha, rt, mt, tad,...
+          'VariableNames',{'id', 'group', 'cycle', 'ha', 'rt', 'mt', 'tad'});
+writetable(T, fullfile(cd, "Statistics", "Data", "Data_Reaching.xlsx"));
